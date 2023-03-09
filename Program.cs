b077aaf4f50f9aa8e5a1af916790d83bd30cdefd
myrshs <127387254+myrshs@users.noreply.github.com>
2023-03-09 10:58:44 +0200

    1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[6];
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write($"array[{i}]=");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for(int i=0;i<0;i++)
+                {
+                    Console.WriteLine($"array[{i}]={array[i]}");
+                }
+            Console.Read();
+        }
+    }
+}
