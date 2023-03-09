16f2a1226ffb1933c0d1ea8c89ef1e2a6765715b
myrshs <127387254+myrshs@users.noreply.github.com>
2023-03-09 11:15:31 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -20,3 +20,15 @@ namespace ConsoleApp1
         }
     }
 }
+
+            int[] array = new int[12];
+            Random rnd = new Random();
+            for (int i = 0; i < 12; i++)
+            {
+                array[i] = rnd.Next(163, 190);
+            }
+            for (int i = 0; i < 12; i++)
+            {
+                Console.WriteLine($"array[{i}]={array[i]}");
+            }
+            Console.Read();
